433dc1a112329a48981297bfa5f6293ae4d0a0c1
Lok-Ming Chan <74899366+Lokmingchan@users.noreply.github.com>
2021-05-14 15:42:58 -0400

    Add files via upload

---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using ProjectEuler.Interfaces;
+
+namespace ProjectEuler
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string answer;
+            
+            do
+            {
+                Console.Write("Which problem's solution would you like to see: ");
+                answer = Console.ReadLine();
+
+                if (!int.TryParse(answer, out int numProblem)) continue;
+                Console.WriteLine();
+                GetSolution(numProblem);
+                Console.WriteLine();
+            } while (answer != "exit" && answer != "quit" && answer != "q" && answer != "x");
+        }
+
+        private static void GetSolution(int numProblem)
+        {
+            IProblem prob = null;
+            string padNum = numProblem.ToString().PadLeft(3, '0');
+            Type type = Type.GetType("ProjectEuler.Problems.Problem" + padNum);
+            prob = (IProblem)Activator.CreateInstance(type);
+
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            prob.Solution();
+            stopWatch.Stop();
+            Console.WriteLine("Elapsed Time: " + stopWatch.Elapsed + " / " + stopWatch.ElapsedTicks + " ticks");
+        }
+
+    }
+}
